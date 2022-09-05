61bcc406c225cdcdffb8e07d672b164ae886980f
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-09-05 14:03:52 -0300

    Entrega do Exercicio01

---
+++ exercicio1/script.js
@@ -0,0 +1,29 @@
+const sacolao = []
+
+// 03 objetos de frutas
+const fruta01 = {
+    nomeFruta: "banana",
+    precoFruta: 15.00,
+    disponibilidadeFruta: true
+}
+
+const fruta02 = {
+    nomeFruta: "maça",
+    precoFruta: 20.00,
+    disponibilidadeFruta: true
+}
+
+const fruta03 = {
+    nomeFruta: "manga",
+    precoFruta: 15.00,
+    disponibilidadeFruta: false
+}
+
+sacolao.push(fruta01)
+sacolao.push(fruta02,fruta03)
+
+
+
+
+console.log (sacolao)
+console.lg (sacola(0))
